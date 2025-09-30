a3b6a93c9e43860fbc590e73280c189fd1144bf5
Alvaro Muñoz Zurita <almn.alvaromunoz@cbtis66.edu.mx>
2025-09-30 11:49:04 -0600

    prueba

--- loginVulnerable/config_vulnerable.php
+++ loginVulnerable/config_vulnerable.php
@@ -3,6 +3,6 @@ class VulnerableConfig {
     public const DB_HOST = 'localhost';
     public const DB_NAME = 'ejercicio1';
     public const DB_USER = 'root';
-    public const DB_PASS = '123456';
+    public const DB_PASS = 'alvaro';
 }
 
